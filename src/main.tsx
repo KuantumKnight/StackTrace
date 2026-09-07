a7065b5e6a0d388f03567d1eea440d48540538b1
KuantumKnight <msarvesh.dav@gmail.com>
2026-09-07 15:25:16 +0530

    style: introduce signal lab visual system

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 import './styles/release-polish.css'
-import './styles/neo-brutalist.css'
+import './styles/signal-lab.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode><App /></React.StrictMode>,
