4860d3b558c717680fdddd423640d67447c62097
Sarvesh M <msarvesh.dav@gmail.com>
2026-09-08 08:08:16 +0530

    style(build): load workbench overrides after global themes

--- src/main.tsx
+++ src/main.tsx
@@ -4,6 +4,7 @@ import App from './App'
 import { ExperienceLayer } from './components/ExperienceLayer'
 import './styles/human-polish.css'
 import './styles/experience.css'
+import './styles/build-workbench.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
