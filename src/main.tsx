89bca2415ebbf75e5c3aa94c464192f31cdce4a9
Sarvesh M <msarvesh.dav@gmail.com>
2026-09-08 07:57:13 +0530

    feat(ui): mount global experience layer

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,13 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
+import { ExperienceLayer } from './components/ExperienceLayer'
 import './styles/human-polish.css'
+import './styles/experience.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode><App /></React.StrictMode>,
+  <React.StrictMode>
+    <App />
+    <ExperienceLayer />
+  </React.StrictMode>,
 )
